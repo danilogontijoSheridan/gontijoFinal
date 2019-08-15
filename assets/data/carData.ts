7cfe6e91bd78576a1ad7320737c3d0cfca2fd06b
danilogontijoSheridan <52791736+danilogontijoSheridan@users.noreply.github.com>
2019-08-15 11:40:57 -0400

    Add files via upload

---
+++ assets/data/carData.ts
@@ -0,0 +1,9 @@
+import {Car} from "../../app/Car";
+
+export const CARDATA : Car[] = [
+
+   { model:"Ferrari",  engine:"V-12 Turbo",    color:"Red"},
+   { model:"VW Beatle",  engine:"Electric",    color:"Black"}
+
+
+]
